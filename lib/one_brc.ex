da9bd59c3adf21c4d52568dfb6d4c1b161d098e7
andy <andyho280392@gmail.com>
2024-01-15 19:32:21 +1100

    init

---
+++ lib/one_brc.ex
@@ -0,0 +1,18 @@
+defmodule OneBrc do
+  @moduledoc """
+  Documentation for `OneBrc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OneBrc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/one_brc_test.exs
@@ -0,0 +1,8 @@
+defmodule OneBrcTest do
+  use ExUnit.Case
+  doctest OneBrc
+
+  test "greets the world" do
+    assert OneBrc.hello() == :world
+  end
+end
